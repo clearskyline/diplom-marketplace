f5c333e0723ae8aa836c6842fa58fd3494062c1b
clearskyline <dmgordeyev@outlook.com>
2023-06-16 07:30:00 +0300

    DRF THROTTLING ADDDED

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -170,6 +170,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2,
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 }
 
 
